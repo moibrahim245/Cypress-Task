d40cb5d7b6c16050d76dcac3114422bee72791c8
mohamed <mohamed.upwork245@gmail.com>
2023-07-13 17:05:42 +0300

    add singUp Scenario

---
+++ cypress/integration/addToCard.cy.js
@@ -0,0 +1,34 @@
+const { bakery } = require("../pageObject/bakery.page");
+const { createAccount } = require("../pageObject/createAccount.page");
+const { homePage } = require("../pageObject/homePage.page");
+const { identification } = require("../pageObject/identification.page");
+const { generatePhoneNumber, generateSignUpData } = require("../support/helper/utils");
+const { customerUserInfo } = require("../testData/customerUser");
+
+
+describe('empty spec', () => {
+  beforeEach(()=>{
+    cy.clearLocalStorage();
+    cy.clearCookies();
+    cy.setCookie('newsletter', "1");
+  })
+  it('[TC001] SignUp with valid credentials', () => {
+    const signUpData = generateSignUpData();
+    homePage.visitHomePage();
+    homePage.NavigateToIdentificationPage();
+    identification.typeValidEmail(signUpData.email);
+    identification.clickContinueButton()
+    createAccount.typeValidPassword(signUpData.password)
+    createAccount.confirmPassword(signUpData.password)
+    createAccount.clickContinueButtonInPasswordCard()
+    createAccount.typeFirstName(signUpData.firstName)
+    createAccount.typeLastName(signUpData.lastName);
+    createAccount.typePhoneNumber(signUpData.phoneNumber)
+    createAccount.clickContinueButtonInProfileDetails();
+    createAccount.selectGender(signUpData.gender)
+    createAccount.selectBirthDate(signUpData.birthDate);
+    createAccount.acceptTermAndConditions();
+    createAccount.clickContinueButtonInGenderCard()
+    createAccount.clickGetStart();
+  })
+})
