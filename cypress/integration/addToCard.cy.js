04360bfcd1b6fc38d1645cac5631c21beaa99aec
moibrahim245 <39190050+moibrahim245@users.noreply.github.com>
2023-07-14 14:35:57 +0300

    Update addToCard.cy.js

--- cypress/integration/addToCard.cy.js
+++ cypress/integration/addToCard.cy.js
@@ -19,38 +19,47 @@ describe('empty spec', () => {
 	it('[TC001] SignUp with valid credentials', () => {
 		const signUpData = generateSignUpData();
 		homePage.visitHomePage();
+		
 		homePage.NavigateToIdentificationPage();
 		identification.typeValidEmail(signUpData.email);
 		identification.clickContinueButton();
+		
 		createAccount.typeValidPassword(signUpData.password);
 		createAccount.confirmPassword(signUpData.password);
 		createAccount.clickContinueButtonInPasswordCard();
+		
 		createAccount.typeFirstName(signUpData.firstName);
 		createAccount.typeLastName(signUpData.lastName);
 		createAccount.typePhoneNumber(signUpData.phoneNumber);
 		createAccount.clickContinueButtonInProfileDetails();
+		
 		createAccount.selectGender(signUpData.gender);
 		createAccount.selectBirthDate(signUpData.birthDate);
 		createAccount.acceptTermAndConditions();
 		createAccount.clickContinueButtonInGenderCard();
+		
 		createAccount.verifyAccountCreated();
 		createAccount.clickGetStart();
 		homePage.verifyUserRedirectedToHomePage();
 	});
 	it('[TC002] add to card with logged in user', { scrollBehavior: false }, () => {
 		homePage.visitHomePage();
+		
 		homePage.NavigateToIdentificationPage();
 		identification.typeValidEmail(customerUserInfo.email);
 		identification.clickContinueButton();
+		
 		createAccount.typeValidPassword(customerUserInfo.password);
 		identification.clickContinueButton();
 		homePage.verifyLoginWelcomeLabel(customerUserInfo.firstName);
+		
 		homePage.hoverOverSuperMarket();
 		homePage.selectBakerySubCategory();
 		bakery.addToCartProductNumber(1);
 		common.verifyProductIsAddedToCart(1);
 		bakery.addToCartProductNumber(2);
 		common.verifyProductIsAddedToCart(2);
+		
 		common.navigateToCartPage();
 		cart.verifySubtotalIsCalculatedCorrectly();
 	});
